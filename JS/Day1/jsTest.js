304c19c10911760cdcfb9db247004860caf1f46e
AungPyaeSoneUCS <mgaungpyaesone98@gmail.com>
2026-09-17 18:27:23 +0630

    Learning Programming

---
+++ JS/Day1/jsTest.js
@@ -0,0 +1,8 @@
+function changeText(){
+    document.getElementById("test").innerHTML = "Hello World!";
+    console.log(5 + 6);
+
+}
+
+
+
---
+++ JS/Day1/styleChange.js
@@ -0,0 +1,38 @@
+
+
+function colorStyling(){
+    document.getElementById('myName').style.color = 'pink'
+}
+
+function backgroundStyling(){
+    document.getElementById('myName').style.backgroundColor = 'yellow'
+}
+
+function fontStyling(){
+    document.getElementById('myName').style.fontSize = '50px'
+    document.getElementById('myName').style.fontFamily = 'Arial'
+}
+
+function textAlignStyling(){
+    document.getElementById('myName').style.textAlign = 'center'
+}
+
+function borderStyling(){
+    document.getElementById('myName').style.border = '1px solid Green'
+}
+
+function paddingStyling(){
+    document.getElementById('myName').style.padding = '10px'
+}
+
+function marginStyling(){
+    document.getElementById('myName').style.margin = '10px'
+}
+
+function hideElement(){
+    document.getElementById('myName').style.display = 'none'
+}
+
+function showElement(){
+    document.getElementById('myName').style.display = 'block'
+}
